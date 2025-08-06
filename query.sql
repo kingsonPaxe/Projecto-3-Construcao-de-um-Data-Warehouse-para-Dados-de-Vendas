108087b8726e7edc0fad31ea4f60c5d7cb0cacd9
KingsonPaxe <jeovanijp972@gmail.com>
2025-08-06 21:36:47 +0100

    Atualizei o arquivo index.ipynb e o query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,29 @@
 -- Criação do banco de dados 'superStore'
 create database superStore;
 --- -------------------------------------------------- Consultas no Sql --------------------------------------------
-select * from facto_vendas; 
+-- Selecionar todas as vendas
+SELECT * FROM facto_vendas;
+
+-- Total de facto_vendas por produto
+SELECT `chaveProduto`, SUM(Valor_venda) AS total_facto_vendas
+FROM facto_vendas
+GROUP BY `chaveProduto`;
+
+-- Total de facto_vendas por cliente
+SELECT `chaveCliente`, SUM(Valor_venda) AS total_gasto
+FROM facto_vendas
+GROUP BY `chaveCliente`;
+
+-- Produtos mais vendidos (top 5)
+SELECT `chaveProduto`, COUNT(*) AS quantidade_vendida
+FROM facto_vendas
+GROUP BY `chaveProduto`
+ORDER BY quantidade_vendida DESC
+LIMIT 5;
+
+-- Clientes que mais compraram (top 5)
+SELECT `chaveCliente`, SUM(Valor_venda) AS total_gasto
+FROM facto_vendas
+GROUP BY `chaveCliente`
+ORDER BY total_gasto DESC
+LIMIT 5;
